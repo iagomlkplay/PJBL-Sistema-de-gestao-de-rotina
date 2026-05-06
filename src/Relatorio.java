ac41432aa1d7eeb2faef49f822d120e3bcfbf098
iagomlkplay <iagofelicha@gmail.com>
2026-05-06 01:50:07 -0300

    Update Relatorio.java

--- src/Relatorio.java
+++ src/Relatorio.java
@@ -1,21 +1,27 @@
 import java.util.Date;
 
 public class Relatorio {
+    private static int ultimoId = 0;
     private int id;
     private Date dataEnvio;
     private String conteudo;
+    private Tarefa tarefaRelacionada;
+    private Projeto projetoRelacionado;
 
-    public Relatorio(int id, Date dataEnvio, String conteudo) {
-        this.id = id;
-        this.dataEnvio = dataEnvio;
+    public Relatorio(String conteudo) {
+        this.id = ++ultimoId;
         this.conteudo = conteudo;
+        this.dataEnvio = new Date();
     }
 
     // Getters e Setters
     public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
     public Date getDataEnvio() { return dataEnvio; }
     public void setDataEnvio(Date dataEnvio) { this.dataEnvio = dataEnvio; }
     public String getConteudo() { return conteudo; }
     public void setConteudo(String conteudo) { this.conteudo = conteudo; }
+    public Tarefa getTarefaRelacionada() { return tarefaRelacionada; }
+    public void setTarefaRelacionada(Tarefa tarefaRelacionada) { this.tarefaRelacionada = tarefaRelacionada; }
+    public Projeto getProjetoRelacionado() { return projetoRelacionado; }
+    public void setProjetoRelacionado(Projeto projetoRelacionado) { this.projetoRelacionado = projetoRelacionado; }
 }
