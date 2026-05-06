a1c40d8d3815ea03cee54f8e909de817b7747c7a
iagomlkplay <iagofelicha@gmail.com>
2026-05-06 01:50:14 -0300

    Update SolicitacaoMudanca.java

--- src/SolicitacaoMudanca.java
+++ src/SolicitacaoMudanca.java
@@ -1,25 +1,29 @@
 import java.util.Date;
 
 public class SolicitacaoMudanca {
+    private static int ultimoId = 0;
     private int id;
     private String justificativa;
     private StatusSolicitacao status;
     private Date dataCriacao;
+    private UsuarioDev solicitante;
 
-    public SolicitacaoMudanca(int id, String justificativa, StatusSolicitacao status, Date dataCriacao) {
-        this.id = id;
+    public SolicitacaoMudanca(String justificativa, UsuarioDev solicitante) {
+        this.id = ++ultimoId;
         this.justificativa = justificativa;
-        this.status = status;
-        this.dataCriacao = dataCriacao;
+        this.status = StatusSolicitacao.PENDENTE;
+        this.dataCriacao = new Date();
+        this.solicitante = solicitante;
     }
 
     // Getters e Setters
     public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
     public String getJustificativa() { return justificativa; }
     public void setJustificativa(String justificativa) { this.justificativa = justificativa; }
     public StatusSolicitacao getStatus() { return status; }
     public void setStatus(StatusSolicitacao status) { this.status = status; }
     public Date getDataCriacao() { return dataCriacao; }
     public void setDataCriacao(Date dataCriacao) { this.dataCriacao = dataCriacao; }
+    public UsuarioDev getSolicitante() { return solicitante; }
+    public void setSolicitante(UsuarioDev solicitante) { this.solicitante = solicitante; }
 }
