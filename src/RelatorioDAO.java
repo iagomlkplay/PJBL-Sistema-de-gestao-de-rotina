918312821ba9d69aee6370e7448affae6c165aae
iagomlkplay <iagofelicha@gmail.com>
2026-05-13 01:46:54 -0300

    Update RelatorioDAO.java

--- src/RelatorioDAO.java
+++ src/RelatorioDAO.java
@@ -43,4 +43,13 @@ public class RelatorioDAO {
         }
         return lista;
     }
+
+    public void deletar(int id) throws SQLException {
+        String sql = "DELETE FROM relatorios WHERE id = ?";
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+        }
+    }
 }
