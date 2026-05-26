51e07b791c05bf9a52a7e6f0fd5f538d9a20e45f
iagomlkplay <iagofelicha@gmail.com>
2026-05-26 11:39:54 -0300

    Create Solicitacao.java

---
+++ src/Solicitacao.java
@@ -0,0 +1,38 @@
+import java.util.Date;
+
+public class Solicitacao {
+    private int id;
+    private String justificativa;
+    private StatusSolicitacao status;
+    private Date dataCriacao;
+    private UsuarioDev solicitante;
+    private Tarefa tarefaRelacionada;  // nova referência
+
+    public Solicitacao(String justificativa, UsuarioDev solicitante, Tarefa tarefaRelacionada) {
+        this.justificativa = justificativa;
+        this.status = StatusSolicitacao.PENDENTE;
+        this.dataCriacao = new Date();
+        this.solicitante = solicitante;
+        this.tarefaRelacionada = tarefaRelacionada;
+    }
+
+    // Getters e Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getJustificativa() { return justificativa; }
+
+    // Justificativa é definida no construtor e nunca alterada. Não precisa realmente existir.
+    public void setJustificativa(String justificativa) { this.justificativa = justificativa; }
+
+    public StatusSolicitacao getStatus() { return status; }
+    public void setStatus(StatusSolicitacao status) { this.status = status; }
+    public Date getDataCriacao() { return dataCriacao; }
+    public void setDataCriacao(Date dataCriacao) { this.dataCriacao = dataCriacao; }
+    public UsuarioDev getSolicitante() { return solicitante; }
+
+    // Solicitante é definida no construtor e nunca alterada. Também não precisa realmente existir.
+    public void setSolicitante(UsuarioDev solicitante) { this.solicitante = solicitante; }
+
+    public Tarefa getTarefaRelacionada() { return tarefaRelacionada; }
+    public void setTarefaRelacionada(Tarefa tarefaRelacionada) { this.tarefaRelacionada = tarefaRelacionada; }
+}
